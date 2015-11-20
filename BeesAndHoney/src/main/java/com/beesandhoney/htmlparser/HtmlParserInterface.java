02950ba1ddc9045b1abfb45dedc5ec29eaaf23e7
azazello <rorschach2109@gmail.com>
2015-11-20 12:15:35 +0100

    Add html parser package

---
+++ BeesAndHoney/src/main/java/com/beesandhoney/htmlparser/HtmlParserInterface.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.beesandhoney.htmlparser;
+
+/**
+ *
+ * @author azazello
+ */
+public interface HtmlParserInterface {
+    public void GetAccountAmounts(String accountAmountsSection);
+    public void GetAccountInformation(String accountInformationSection);
+    public void GetAccountHistory(String accountHistoryInformation);
+}
