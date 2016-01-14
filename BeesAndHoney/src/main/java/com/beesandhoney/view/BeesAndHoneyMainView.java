a179e85b57e27252647be4a254f9a9ed75a22c0b
azazello <rorschach2109@gmail.com>
2016-01-14 16:12:11 +0100

    Add two new views

---
+++ BeesAndHoney/src/main/java/com/beesandhoney/view/BeesAndHoneyMainView.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.beesandhoney.view;
+
+import com.beesandhoney.controller.BeesAndHoneyMainController;
+import com.beesandhoney.controller.IController;
+import com.beesandhoney.model.BankingBookModel;
+import java.util.Date;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.BorderPane;
+
+public class BeesAndHoneyMainView implements IView {
+    
+    private final BeesAndHoneyMainController mainController;
+    
+    @FXML
+    BorderPane mainView;
+    
+    @FXML
+    Label currentUserLabel;
+    
+    @FXML
+    Label lastAccessDateLabel;
+    
+    @FXML
+    TableView<BankingBookModel> bankingBookTable;
+    
+    public BeesAndHoneyMainView() {
+        System.out.println("com.beesandhoney.view.BeesAndHoneyMainView.<init>()");
+        this.mainController = new BeesAndHoneyMainController(this);
+    }
+    
+    public void setDisableFlag(boolean disableFlag) {
+        this.mainView.setDisable(disableFlag);
+    }
+    
+    public void setCurrentUser(String currentUserLogin) {
+        this.currentUserLabel.setText(currentUserLogin);
+    }
+    
+    public void setLastAccessDate(Date lastAccessDate) {
+        this.lastAccessDateLabel.setText(lastAccessDate.toString());
+    }
+    
+    public ObservableList<BankingBookModel> getBankingBookTableItems() {
+        return bankingBookTable.getItems();
+    }
+    
+    @Override
+    public void cleanUp() {
+        
+    }
+    
+    @Override
+    public IController getController() {
+        return this.mainController;
+    }
+    
+    private int getBankingBookTableSelectedIndex() {
+        return this.bankingBookTable.getSelectionModel().getFocusedIndex();
+    }
+    
+    @FXML
+    private void handleDetailsButtonClicked() {
+        System.out.println("com.beesandhoney.view.BeesAndHoneyMainView.handleDetailsButtonClicked()");
+        this.mainController.handleShowBankingBookItemDetails(getBankingBookTableSelectedIndex());
+    }
+    
+    @FXML
+    private void handleRefreshButtonClicked() {
+        System.out.println("com.beesandhoney.view.BeesAndHoneyMainView.handleRefreshButtonClicked()");
+        this.mainController.handleRefreshBankingBookTable();
+    }
+    
+    @FXML
+    private void handleAddButtonClicked() {
+        System.out.println("com.beesandhoney.view.BeesAndHoneyMainView.handleAddButtonClicked()");
+        this.mainController.handleAddBankingBookItem();
+//        ObservableList<BankingBookModel> data = this.bankingBookTable.getItems();
+//        data.add(new BankingBookModel("a", "b", "c"));
+    }
+    
+    @FXML
+    private void handleDeleteButtonClicked() {
+        System.out.println("com.beesandhoney.view.BeesAndHoneyMainView.handleDeleteButtonClicked()");
+        this.mainController.handleDeleteBankingBookItem(getBankingBookTableSelectedIndex());
+    }
+    
+    @FXML
+    private void initialize() {
+    }
+}
---
+++ BeesAndHoney/src/main/java/com/beesandhoney/view/DecisionView.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.beesandhoney.view;
+
+import com.beesandhoney.utils.ObservableInterface;
+import javafx.fxml.FXML;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import com.beesandhoney.utils.ObserverInterface;
+import java.util.ArrayList;
+
+public class DecisionView implements ObservableInterface {
+    
+    boolean decisionResult;
+    ArrayList<ObserverInterface> observers;
+    
+    public DecisionView() {
+        this.observers = new ArrayList<>();
+        this.decisionResult = false;
+    }
+    
+    public boolean getDecisionResult() {
+        return this.decisionResult;
+    }
+    
+    @Override
+    public void registerObserver(ObserverInterface observer) {
+        this.observers.add(observer);
+    }
+    
+    @Override
+    public void deleteObserver(ObserverInterface observer) {
+        this.observers.remove(observer);
+    }
+    
+    @Override
+    public void notifyObservers() {
+        for (ObserverInterface observer : this.observers) {
+            observer.update();
+        }
+    }    
+    
+    private void setDecisionResult(boolean decisionResult) {
+        this.decisionResult = decisionResult;
+        notifyObservers();
+    }
+    
+    @FXML
+    private void handleCancelButtonReleased(KeyEvent keyEvent) {
+        if (keyEvent.getCode().equals(KeyCode.ENTER)) {
+            setDecisionResult(false);
+        }
+    }
+    
+    @FXML
+    private void handleCancelButtonPressed() {
+        setDecisionResult(false);
+    }
+    
+    @FXML
+    private void handleAgreeButtonReleased(KeyEvent keyEvent) {
+        if (keyEvent.getCode().equals(KeyCode.ENTER)) {
+            setDecisionResult(true);
+        }
+    }
+    
+    @FXML
+    private void handleAgreeButtonClicked() {
+        setDecisionResult(true);
+    }
+}
