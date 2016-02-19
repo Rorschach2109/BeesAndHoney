f55544cce8a5255afe1ed85410fe9a8941319e84
azazello <rorschach2109@gmail.com>
2016-02-19 12:54:40 +0100

    web browser state interface

---
+++ BeesAndHoney/src/main/java/com/beesandhoney/browser/state/IWebBrowserState.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.beesandhoney.browser.state;
+
+import com.beesandhoney.model.BankAccount;
+import com.beesandhoney.model.BankAccountLogin;
+import com.beesandhoney.utils.ObservableInterface;
+import com.beesandhoney.utils.ObserverInterface;
+import javafx.scene.web.WebEngine;
+
+public interface IWebBrowserState extends ObservableInterface {
+    
+    public void getAccountInformation(BankAccountLogin bankAccountLogin);
+    public BankAccount getStashedAccountInformation();
+    
+    public void setWebEngine(WebEngine webEngine);
+    
+    public void logout();
+    
+    @Override
+    public void registerObserver(ObserverInterface observer);
+    
+    @Override
+    public void deleteObserver(ObserverInterface observer);
+    
+    @Override
+    public void notifyObservers();
+}
